01186c896daa93eb74da6a64cb6cd66c2718e106
Alex Jordan <alexander3223098@gmail.com>
2013-10-26 14:27:56 -0700

    add initial resources for autonomous generation

---
+++ tools/TelemetryGenAutonomous/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='templater',
+	version='0.1',
+	description='Take a dump from an FTC teliop and turn it into an autonomous.',
+	author='Alex Jordan',
+	author_email='alexander3223098@gmail.com',
+	url='https://github.com/saasrobotics/telemetry',
+	requires=['pystache']
+     )
