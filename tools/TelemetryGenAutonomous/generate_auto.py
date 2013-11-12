9a73d884fcd1e35c41495c40201d8dbe5f7f8dc4
Alex Jordan <alexander3223098@gmail.com>
2013-11-12 14:43:06 -0800

    do a little more work on the python script

--- tools/TelemetryGenAutonomous/generate_auto.py
+++ tools/TelemetryGenAutonomous/generate_auto.py
@@ -4,6 +4,24 @@ import re
 
 def evalReplacement(str, letterId, replacement):
 	str = re.sub(r'/\*INSP' + letterId + '\*/', replacement, str)
+	#return str
+
+def genArray(encoders):	
+	string = "{"
+	j = 0
+        print("encoder length is " + str(len(encoders)))
+        # TODO refactor this loop, it could be prettier
+	for i in encoders:
+                j += 1
+        	string += str(i)
+                # special case, for if we're at the end of the data
+                # TODO fix this to use the array length somehow
+		if (j != len(encoders)):
+			string += ", "
+		else:
+			string += "}"
+
+                print(string)
 
 # open and read the file. for now, this assumes this file is read from the same directory.
 # TODO: accept arguments for what file to operate on.
@@ -15,5 +33,6 @@ templatestr = templatefd.read()
 encoderDataLength = 5
 
 # insert the length of the array into the template
-evalReplacement(templatestr, 'A', str(encoderDataLength))
+print evalReplacement(templatestr, 'A', str(encoderDataLength))
 
+print genArray([1, 2, 3, 4])
