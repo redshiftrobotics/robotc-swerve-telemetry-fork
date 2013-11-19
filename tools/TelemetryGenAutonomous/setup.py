744f227a6c8acfba3b959441291c77cee0b6ae7c
Alex Jordan <alexander3223098@gmail.com>
2013-11-19 14:54:15 -0800

    don't require pystache

--- tools/TelemetryGenAutonomous/setup.py
+++ tools/TelemetryGenAutonomous/setup.py
@@ -2,11 +2,11 @@
 
 from distutils.core import setup
 
-setup(name='templater',
+setup(name='TelemetryGenAutonomous',
 	version='0.1',
 	description='Take a dump from an FTC teliop and turn it into an autonomous.',
 	author='Alex Jordan',
 	author_email='alexander3223098@gmail.com',
 	url='https://github.com/saasrobotics/telemetry',
-	requires=['pystache']
+	requires=[]
      )
